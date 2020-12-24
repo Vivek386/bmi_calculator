a5117868336797c667b305f3043275b709526a2b
Vivek386 <vivekkumarmahto1998@gmail.com>
2020-12-25 03:23:53 +0530

    BMI

---
+++ lib/constants.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+
+const kBottomContainerHeight = 80.0;
+const kActivecardcolor = Color(0xFF1D1E33);
+const kInactivecardcolor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+
+
+const kLabelTextStyle =TextStyle(
+  fontSize: 20.0,
+  color: Color(0xFF8DBE98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+    fontSize: 25.0,
+    fontWeight: FontWeight.bold
+);
+
+const kTitleTextStyle = TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.white
+);
+
+const kResultTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.green,
+);
+
+const kBMITextStyle = TextStyle(
+    fontSize: 100.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.white
+);
+
+const kBMIBodyTextStyle = TextStyle(
+  fontSize: 20.0,
+);
+
+
+
